2a747b64fdedd5f6719ca335482708829824e1ab
Mihai Avram <mihaiavram72@gmail.com>
2021-08-03 19:45:42 +0300

    Logos added

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,12 +1,15 @@
 jQuery(function() {
     $('.logo-carousel').slick({
-      slidesToShow: 6,
-      slidesToScroll: 1,
+      slidesToShow: 4,
+      slidesToScroll: 2,
       autoplay: true,
       autoplaySpeed: 1000,
+      infinite: true,
       arrows: true,
       dots: false,
       pauseOnHover: false,
+      variableWidth: true,
+      centerMode: true,
       responsive: [{
         breakpoint: 768,
         settings: {
